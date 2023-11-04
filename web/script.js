b5dde4ce170b8d4160f0cf256a670a908a9c9b98
Dilshan Rathnayaka189 <142502900+Dilshan189@users.noreply.github.com>
2023-11-04 21:40:22 +0530

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,11 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
+
+alert('Hellow , World Come to my personal Blog !!!')
